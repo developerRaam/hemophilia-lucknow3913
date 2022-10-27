e83aec2a6188fdab067fc3f2aca3c8c664879de7
ramnarayan maurya <rnm312000@gmail.com>
2022-10-27 20:15:04 +0530

    update

--- project/settings.py
+++ project/settings.py
@@ -132,9 +132,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
